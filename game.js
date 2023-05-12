4b4501f7004f4af96587c7c2c2de3a4a30192e14
abdellahhn <abdellahhanane44@gmail.com>
2023-05-12 14:35:32 -0400

    spaceShip move

--- game.js
+++ game.js
@@ -1,4 +1,34 @@
-var img = document.createElement("img");
-img.src = "../SpaceShipWar/image/SPACESHIP.png";
-var div = document.getElementById("ship")
-div.appendChild(img);
+var animer, left = 0, imgObj = null;
+
+function init() {
+    imgObj = document.getElementById("spaceship");
+    imgObj.style.position = "absolute";
+    imgObj.style.top = '240px';
+    imgObj.style.left = '-300px';
+    imgObj.style.visibility = 'hidden';
+
+    roundMove();
+}
+
+function roundMove() {
+    left = parseInt(imgObj.style.left, 10);
+
+    if (10 >= left) {
+        imgObj.style.left = (left + 5) + 'px';
+        imgObj.style.visibility = 'visible';
+
+        animer = setTimeout(function () {
+            roundMove();
+        }, 20);
+    } else {
+        stop();
+    }
+}
+
+function stop() {
+    clearTimeout(animer);
+}
+
+// window.onload = function() {
+//     init();
+// };
